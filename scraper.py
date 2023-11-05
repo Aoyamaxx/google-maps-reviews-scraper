aed06800133dd1486874db4e9f117ff8ca441b96
Aoyamaxx <tomako.maketo@gmail.com>
2023-11-06 00:48:25 +0100

    Single location scraping complete

--- scraper.py
+++ scraper.py
@@ -5,35 +5,134 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime
 import time
+import csv
 
-def click_sort_option(driver, option):
+def scroll_and_expand_reviews(driver, timeout=2, max_scroll_attempts=10):
+    actions = ActionChains(driver)
+    previous_length = 0
+    scroll_attempts = 0
+    scroll_counter = 0
+
+    while scroll_attempts < max_scroll_attempts:
+        # Scroll down once
+        actions.send_keys(Keys.PAGE_DOWN).perform()
+        scroll_counter += 1
+        time.sleep(timeout)  # Wait for the page to potentially load more items
+
+        # Try to click all "Show More" buttons to expand truncated reviews
+        show_more_buttons = driver.find_elements(By.CSS_SELECTOR, ".w8nwRe.kyuRq")
+        for button in show_more_buttons:
+            try:
+                driver.execute_script("arguments[0].click();", button)
+            except:
+                pass  # Ignore errors if button is not clickable or not in view
+
+        # Extract the current set of reviews
+        current_reviews = driver.find_elements(By.CLASS_NAME, "wiI7pd")
+
+        # Check if the number of reviews has increased
+        if len(current_reviews) > previous_length:
+            previous_length = len(current_reviews)
+            scroll_attempts = 0  # Reset the counter since new reviews were found
+        else:
+            scroll_attempts += 1  # Increment the counter if no new reviews are found
+
+        # Wait a bit before the next scroll attempt
+        time.sleep(timeout)
+        
+    scroll_attempts2 = 0
+
+    while scroll_attempts2 <= scroll_counter:
+        # Scroll up once
+        actions.send_keys(Keys.PAGE_UP).perform()
+        time.sleep(timeout)  # Wait for the page to potentially load previous items
+        scroll_attempts2 += 1
+
+def safe_extract(element, class_name, attribute=None):
+    """Safely extract information from an element, return 'NA' if not found."""
+    try:
+        if attribute:
+            return element.find_element(By.CLASS_NAME, class_name).get_attribute(attribute)
+        else:
+            return element.find_element(By.CLASS_NAME, class_name).text
+    except NoSuchElementException:
+        return 'NA'
+
+def scroll_and_extract_reviews(driver, csv_filename, timeout=2, max_scroll_attempts=10):
+    # Initialize ActionChains
+    actions = ActionChains(driver)
+
+    with open(csv_filename, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        # Define the header with the column names
+        header = ['Location', 'Reviewer', 'Rating', 'Relative time', 'Scraping time', 'Review']
+        # Write the header row
+        writer.writerow(header)
+
+        previous_length = 0
+        scroll_attempts = 0
+
+        while scroll_attempts < max_scroll_attempts:
+            # Scroll down once
+            actions.send_keys(Keys.PAGE_DOWN).perform()
+            time.sleep(timeout)  # Wait for the page to potentially load more items
+
+            # Extract the review container elements
+            review_containers = driver.find_elements(By.CLASS_NAME, "jJc9Ad")
+
+            # Check if the number of review containers has increased
+            if len(review_containers) > previous_length:
+                # Iterate over new reviews since the last time
+                for review_container in review_containers[previous_length:]:
+                    reviewer = safe_extract(review_container, "d4r55")
+                    review_text = safe_extract(review_container, "wiI7pd")
+                    rating = safe_extract(review_container, "kvMYJc", attribute="aria-label")
+                    relative_time = safe_extract(review_container, "rsqaWe")
+                    scraping_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                    # Write the extracted information to the CSV file
+                    writer.writerow(['NA', reviewer, rating, relative_time, scraping_time, review_text])
+
+                previous_length = len(review_containers)
+                scroll_attempts = 0  # Reset the counter since new reviews were found
+            else:
+                scroll_attempts += 1  # Increment the counter if no new reviews are found
+
+            # Wait a bit before the next scroll attempt
+            time.sleep(timeout)
+
+def click_sort_option(driver, option='default'):
+    # If the option is 'default', we do nothing and return immediately
+    if option == 'default':
+        return
+
     # Dictionary mapping the option parameter to the data-index
     sort_options = {
         'most_relevant': '0',
         'newest': '1',
         'highest_rating': '2',
-        'lowest_rating': '3',
+        'lowest_rating': '3'
     }
 
     try:
-        # Find all elements with the class name 'DVeyrd'
+        # Click the sort button to open the submenu
         sort_buttons = WebDriverWait(driver, 10).until(
             EC.presence_of_all_elements_located((By.CLASS_NAME, 'DVeyrd'))
         )
-
-        # Click the second sort button (index 1 since indexing starts at 0)
         sort_buttons[2].click()
 
         # Wait for a short delay to allow the submenu to animate in, if necessary
-        time.sleep(2)  # Adjust the delay time as needed
+        time.sleep(2)
 
         # Wait for the submenu to appear and find the option with the correct data-index
         submenu_option = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, f".fxNQSd[data-index='{sort_options[option]}']"))
         )
-
-        # Click the specified submenu option
         submenu_option.click()
 
     except KeyError:
@@ -44,8 +143,8 @@ def click_sort_option(driver, option):
 try:
     # Setup Chrome options
     chrome_options = Options()
-    chrome_options.add_argument("--lang=en")  # Set language to English
-    chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
+    chrome_options.add_argument("--lang=en-GB")  # Set language to English
+    # chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
     # chrome_options.add_argument("--headless")  # Uncomment if you want to run Chrome in headless mode
 
     # Initialize the Chrome driver
@@ -66,10 +165,14 @@ try:
     # Click the close_button
     close_button.click()
     
-    time.sleep(2)
-    
     # Call the click_sort_option function to sort the reviews by 'newest'
     click_sort_option(driver, 'newest')
+    
+    # Scroll and click "Show More" until all reviews are expanded or until 10 scroll attempts with no new reviews
+    scroll_and_expand_reviews(driver, timeout=0.1, max_scroll_attempts=10)
+
+    # Extract the reviews and write them to a CSV file
+    scroll_and_extract_reviews(driver, 'reviews.csv', timeout=0.1)
 
     # Keep the window open, or perform some actions, or wait for some element or user input
     input("Press Enter to quit...")  # Keep the window open until user presses Enter
